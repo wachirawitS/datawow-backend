f9dbb846330a476c9bd1335dc03d5770105e37dc
wachirawit <wachirawit.ntsi@gmail.com>
2026-05-08 13:50:23 +0700

    feat: enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
